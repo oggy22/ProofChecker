ab469545934bc4010f1c0c7866d11c2ac5e3de5f
oggy <oggy22@gmail.com>
2015-03-11 22:41:59 -0700

    empty line adding

---
+++ ProofCheckerTest/files_test.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+
+#include "..\ProofChecker\lexer.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProofCheckerTest
+{
+	TEST_CLASS(files_test)
+	{
+	public:
+		void test(char* file_name)
+		{
+
+		}
+
+		TEST_METHOD(file_group_test)
+		{
+			test("group.thr");
+
+		}
+	};
+}
